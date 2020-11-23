2cd2dd6d7257471a96f963af4a47def0581aa7f3
Billy Morrison <billy.morrison1234@hotmail.co.uk>
2020-11-23 22:11:18 +0000

    added GET functionality to the search bar, calling the nasa api on click

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,20 @@
 import { useState } from 'react';
 import '../styles/Search.css';
+import getImages from '../requests/getImages.js'
 
 const Search = () => {
     const [value, setValue] = useState('')
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        getImages(value)
+    }
 
     return (
         <>
             <form>
                 <input type='text' data-testid='text-input' onInput={(e) => setValue(e.target.value)}/>
-                <button className='search-button' type='submit'>Search</button>
+                <button className='search-button' type='submit' onClick={(e) => handleSubmit(e)}>Search</button>
             </form>
         </>
     )
